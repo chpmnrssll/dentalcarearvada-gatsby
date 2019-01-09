87e4b7058a293c50dbc4c99a0af7ce9681d88fb3
Russell Chapman <chpmnrssll@gmail.com>
2019-01-09 01:11:12 -0700

    Update favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,15 +24,15 @@ const TemplateWrapper = ({children}) => (<StaticQuery query={graphql `
       <html lang="en"/>
       <title>{data.site.siteMetadata.title}</title>
       <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32"/>
-      <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16"/>
+      <link rel="icon" type="image/png" href="/img/favicon.png" sizes="32x32"/>
+      <link rel="icon" type="image/png" href="/img/favicon.png" sizes="16x16"/>
       <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400"/>
 
       <meta name="theme-color" content="#fff"/>
       <meta name="description" content={data.site.siteMetadata.description}/>
       <meta property="og:url" content="/"/>
       <meta property="og:type" content="business.business"/>
-      <meta property="og:image" content="/img/og-image.jpg"/>
+      {/*<meta property="og:image" content="/img/og-image.jpg"/>*/}
       <meta property="og:title" content={data.site.siteMetadata.title}/>
     </Helmet>
     <section className="hero is-medium">
