805d571153d16e17c5e519dde81821771643dee2
Russell Chapman <chpmnrssll@gmail.com>
2019-01-16 19:13:25 -0700

    Fix netlify build error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,7 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
-  {/* Overwrite default title with 404 */}
-  <Helmet>
-    <title>404</title>
-    <meta name="title" content="404"/>
-    <meta itemprop="name" content="404"/>
-    <meta property="og:title" content="404"/>
-  </Helmet>
   <Layout>
     <h3>NOT FOUND</h3>
     <p>You just hit a route that doesn&#39;t exist...</p>
