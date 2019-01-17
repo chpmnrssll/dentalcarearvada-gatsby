8da53e8678e943b7ee37fb9ad44f052011b79bbe
Russell Chapman <chpmnrssll@gmail.com>
2019-01-16 19:04:36 -0700

    Updated meta data to mimic all tags from the old site.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,9 @@ import DesktopTabBar from '../components/DesktopTabBar'
 import SideBar from '../components/SideBar'
 import Footer from '../components/Footer'
 
-import imgLogo from '../img/customLogoa0e8.jpg'
+import imgLogo from '../img/customLogoa0e8_retouched.jpg'
+import imgFavicon from '../img/favicon.png'
+import imgSmileMore from '../img/smile_more.jpg'
 
 const TemplateWrapper = ({children}) => (<StaticQuery query={graphql `
     query HeadingQuery {
@@ -22,18 +24,26 @@ const TemplateWrapper = ({children}) => (<StaticQuery query={graphql `
   `} render={data => (<div>
     <Helmet>
       <html lang="en"/>
-      <title>{data.site.siteMetadata.title}</title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" href="/img/favicon.png" sizes="32x32"/>
-      <link rel="icon" type="image/png" href="/img/favicon.png" sizes="16x16"/>
-      <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400"/>
-
-      <meta name="theme-color" content="#fff"/>
-      <meta name="description" content={data.site.siteMetadata.description}/>
       <meta property="og:url" content="/"/>
       <meta property="og:type" content="business.business"/>
-      {/*<meta property="og:image" content="/img/og-image.jpg"/>*/}
+      <meta name="theme-color" content="#fff"/>
+
+      <title>{data.site.siteMetadata.title}</title>
+      <meta name="title" content={data.site.siteMetadata.title}/>
+      <meta itemprop="name" content={data.site.siteMetadata.title}/>
       <meta property="og:title" content={data.site.siteMetadata.title}/>
+
+      <meta name="description" content={data.site.siteMetadata.description}/>
+      <meta itemprop="description" content={data.site.siteMetadata.description}/>
+      <meta property="og:description" content={data.site.siteMetadata.description}/>
+
+      <meta itemprop="image" content={imgSmileMore}/>
+      <meta property="og:image" content={imgSmileMore}/>
+
+      <link rel="shortcut icon" type="image/x-icon" href={imgFavicon}/>
+      <link rel="apple-touch-icon" sizes="180x180" href={imgFavicon}/>
+      <link rel="icon" type="image/png" href={imgFavicon} sizes="32x32"/>
+      <link rel="icon" type="image/png" href={imgFavicon} sizes="16x16"/>
     </Helmet>
     <section className="hero is-medium">
       <img className="image heroImage" src={imgLogo} alt="Dr. Gregory Waters, DDS"/>
